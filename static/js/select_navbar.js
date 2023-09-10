3e3a4f295def5996445788fbf5ee474a6e90ebf6
birobalint95 <87605943+birobalint95@users.noreply.github.com>
2023-09-10 16:37:06 +0200

    get url in js

--- static/js/select_navbar.js
+++ static/js/select_navbar.js
@@ -1,4 +1,13 @@
 $(document).ready (function() {
     const url_var = document.URL;
     console.log(url_var);
+
+    const host_name = window.location.hostname;
+    console.log(host_name);
+
+    const href_name = window.location.href;
+    console.log(href_name);
+
+    const hash_name = window.location.hash;
+    console.log(hash_name);
 });
